32e95711cb03703b1fa49a285d8edff348831d86
Olivier Nizet <onizet@hotmail.com>
2024-11-10 19:06:32 +0100

    Supports a feature to disable heading numbering #175

--- test/HtmlToOpenXml.Tests/HeadingTests.cs
+++ test/HtmlToOpenXml.Tests/HeadingTests.cs
@@ -51,6 +51,32 @@ namespace HtmlToOpenXml.Tests
             });
         }
 
+        [TestCase("<h1>1. Heading 1</h1><h2>1.1 Heading Normal Case</h1>")]
+        [TestCase("<h1>1. Heading 1</h1><h2>1.1  Heading Double Space</h2>", Description = "Double space after number")]
+        [TestCase("<h1>1. Heading 1</h1><h2>1.2&#09;Heading Tab</h2>", Description = "Tab after number")]
+        [TestCase("<h1>1. Heading 1</h1><h2>1.3Heading No Space</h2>", Description = "No space after number")]
+        public void OrderedPattern_DisableNumberingSupports_ReturnsSimpleHeading(string html)
+        {
+            converter.SupportsHeadingNumbering = false;
+            var elements = converter.Parse(html);
+
+            var absNum = mainPart.NumberingDefinitionsPart?.Numbering
+                .Elements<AbstractNum>()
+                .Where(abs => abs.AbstractNumDefinitionName?.Val == NumberingExpressionBase.HeadingNumberingName)
+                .SingleOrDefault();
+            Assert.That(absNum, Is.Null);
+
+            var paragraphs = elements.Cast<Paragraph>();
+            Assert.Multiple(() =>
+            {
+                Assert.That(paragraphs.Count(), Is.EqualTo(2));
+                Assert.That(paragraphs.First().InnerText, Is.EqualTo("1. Heading 1"));
+                Assert.That(paragraphs.First().ParagraphProperties?.NumberingProperties?.NumberingLevelReference?.Val,
+                    Is.Null,
+                    "First paragraph is not a numbering");
+            });
+        }
+
         [Test]
         public void MaxLevel_ShouldBeIgnored()
         {
